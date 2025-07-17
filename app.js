bafc363385557e038004eff1e1db809e7a30bce5
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-17 15:40:47 +0530

    number

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 console.log('Hello, World!');
 var a = 5;
 console.log(a);
+let arr = [1, 2, 3];
+console.log(arr);
--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
 console.log('Hello, World!');
 var a = 5;
 console.log(a); 
+
+let arr :number[] = [1, 2, 3];
+console.log(arr);
