ff521b95e375181ae1058442d22b98f87e2229ce
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-07-18 12:12:09 +0530

    unknown

--- app.ts
+++ app.ts
@@ -26,3 +26,9 @@ console.log(charecters.BLEACH);
 let b ;
 
 let c : number = 10; 
+
+let d :unknown;
+d = 20;
+d = 'hello';
+
+d.toString(); // This will cause an error if uncommented, as `unknown` type does not have a defined method. 
